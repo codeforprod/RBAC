c5e6e09988263ebab582e59188261c3b6349059b
Oleksii Popov <popov.scar@gmail.com>
2026-01-09 13:29:54 +0000

    fix: remove project reference from ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,8 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
     sourceType: 'module',
+    ecmaVersion: 2022,
   },
   plugins: ['@typescript-eslint', 'prettier'],
   extends: [
